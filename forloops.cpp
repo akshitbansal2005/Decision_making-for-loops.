6c9a5e2266cc2614a9fc2cf34845f7ad5bdaf180
Akshit Bansal <155195875+akshitbansal2005@users.noreply.github.com>
2024-09-25 13:31:42 +0530

    Create forloops.cpp

---
+++ forloops.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    for (i=0;i<5;i++)
+    {
+        cout<<"SIT"<<endl;
+    }
+    return 0;
+}
+
+/*Output
+SIT
+SIT
+SIT
+SIT
+SIT*/
